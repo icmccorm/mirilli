0d2c016f8742553590f1134fd0addad91ebefec4
Ian McCormack <icmccorm@andrew.cmu.edu>
2022-11-16 11:02:54 -0500

    Added shebang.

--- check.sh
+++ check.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 NUM_VISITED=$(wc -l < ./data/results/8/visited.csv); bc -l <<< "$NUM_VISITED/93477.0*100"
--- clean.sh
+++ clean.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 NIGHTLY_VERSION="nightly-2022-09-29"
 REGEX="/$NIGHTLY_VERSION/d"
 EXCESS_TOOLCHAINS="$(rustup toolchain list | sed '/(default)/d' | sed $REGEX)"
+
 if ( test "$(echo $EXCESS_TOOLCHAINS | wc -l)" -gt "1" ); then
     while IFS= read -r line; do 
         rustup toolchain uninstall $line
