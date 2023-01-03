bc25cebc457eabb79a7109bd50c0d171ca83522e
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-01-03 14:17:50 -0600

    Created audit book, corrected sampling, and refactored execution script for data collection.

--- exec.sh
+++ exec.sh
@@ -4,34 +4,27 @@ RED='\e[1;31m'
 GREEN='\e[1;32m'
 NC='\e[1;0m'
 make clean && make all
-mkdir -p "$2"
-mkdir -p "$2/$1"
-mkdir -p "$2/$1/early"
-mkdir -p "$2/$1/late"
-TO_VISIT=""
-if test -f "$2/$1/visited.csv"; then
-    cat "$2/$1/visited.csv" | sort > ./visited_sorted.csv;
-    cat "./data/partitions/p$1.csv" | sort > ./all_sorted.csv;
-    TO_VISIT=$(comm -23 "./all_sorted.csv" "./visited_sorted.csv")
-else
-    TO_VISIT=$(cat ./data/partitions/p$1.csv)
-fi
+rm -rf ./data/results
+mkdir -p ./data/results
+mkdir -p ./data/results/early
+mkdir -p ./data/results/late
+TO_VISIT=$(cat ./data/all.csv)
 while IFS=, read name version; 
-do 
+do
     if (cargo-download -x "$name==$version" --output ./test 1> /dev/null); then 
 	if ! (cd test && (timeout 5m cargo dylint --all 1> /dev/null)); then
-            echo "Writing failure to $2/$1/failed_compilation.csv"
-            echo "$name,$version,$?" >> "$2/$1/failed_compilation.csv"
+            echo "Writing failure to data/results/failed_compilation.csv"
+            echo "$name,$version,$?" >> "data/results/failed_compilation.csv"
         fi
-        echo "Writing visit to $2/$1/visited.csv"
-        echo "$name,$version" >> "$2/$1/visited.csv"
-        echo "Copying analysis output to $2/$1/early/$name.json"
-        [ ! -f ./test/ffickle_early.json ] || mv ./test/ffickle_early.json "$2/$1/early/$name.json"
-        echo "Copying analysis output to $2/$1/late/$name.json"
-        [ ! -f ./test/ffickle_late.json ] || mv ./test/ffickle_late.json "$2/$1/late/$name.json"
+        echo "Writing visit to data/results/visited.csv"
+        echo "$name,$version" >> "data/results/visited.csv"
+        echo "Copying analysis output to data/results/early/$name-$version.json"
+        [ ! -f ./test/ffickle_early.json ] || mv ./test/ffickle_early.json "data/results/early/$name-$version.json"
+        echo "Copying analysis output to data/results/late/$name-$version.json"
+        [ ! -f ./test/ffickle_late.json ] || mv ./test/ffickle_late.json "data/results/late/$name-$version.json"
     else
-        echo "${RED}DOWNLOAD FAILED${NC} $name\n"
-        echo "$name,$version,$?" >> "$2/$1/failed_download.csv"
+        echo "${RED}DOWNLOAD FAILED${NC} $name $version\n"
+        echo "$name,$version,$?" >> "data/results/failed_download.csv"
     fi
     rm -rf ./test
 done <<< "$TO_VISIT"
