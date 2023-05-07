0d9c3da3bf72014639e53edc35e3dc80ab2578ec
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-05-07 09:58:30 -0400

    Updated to 1.71.

--- src/early/src/lib.rs
+++ src/early/src/lib.rs
@@ -17,16 +17,16 @@ extern crate rustc_parse;
 extern crate rustc_span;
 extern crate rustc_target;
 extern crate rustc_trait_selection;
-use std::io::Write;
+use crate::rustc_lint::LintContext;
 use rustc_ast::ast;
 use rustc_ast::visit::FnKind;
-use rustc_target::spec::abi::lookup;
 use rustc_ast::Extern::Explicit;
 use rustc_span::Span;
+use rustc_target::spec::abi::lookup;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use crate::rustc_lint::LintContext;
 use shared::*;
+use std::collections::HashMap;
+use std::io::Write;
 
 dylint_linting::impl_early_lint! {
     pub FFICKLE_EARLY,
@@ -42,68 +42,58 @@ struct FfickleEarly {
     foreign_function_abis: HashMap<String, Vec<String>>,
     static_item_abis: HashMap<String, Vec<String>>,
     rust_function_abis: HashMap<String, Vec<String>>,
-    unknown_abis: Vec<String>
+    unknown_abis: Vec<String>,
 }
 
-impl<'tcx> EarlyLintPass for FfickleEarly {
-
-    fn check_fn(&mut self, cx: &EarlyContext<'_>, kind: FnKind< >, _: Span, _: ast::NodeId) {
-        match kind {
-            FnKind::Fn(_, _, sig, ..) => match sig.header.ext {
-                Explicit(sl, _) => {
-                    let session = &cx.sess();
-                    let abi_string = sl.symbol_unescaped.as_str().to_string().replace("\"", "");
-                    match lookup(&abi_string) {
-                        Some(abi) => {
-                            if !is_internal_abi(abi) {
-                                    self.rust_function_abis
-                                    .entry(abi_string.to_string())
-                                    .and_modify(|e| e.extend(vec![span_to_string(sig.span, session)]))
-                                    .or_insert(vec![span_to_string(sig.span, session)]);
-                            }
-                        }
-                        None => {
-                            self.unknown_abis.extend(vec![abi_string])
+impl EarlyLintPass for FfickleEarly {
+    fn check_fn(&mut self, cx: &EarlyContext<'_>, kind: FnKind, _: Span, _: ast::NodeId) {
+        if let FnKind::Fn(_, _, sig, ..) = kind {
+            if let Explicit(sl, _) = sig.header.ext {
+                let session = &cx.sess();
+                let abi_string = sl.symbol_unescaped.as_str().to_string().replace('\"', "");
+                match lookup(&abi_string) {
+                    Some(abi) => {
+                        if !is_internal_abi(abi) {
+                            self.rust_function_abis
+                                .entry(abi_string.to_string())
+                                .and_modify(|e| e.extend(vec![span_to_string(sig.span, session)]))
+                                .or_insert(vec![span_to_string(sig.span, session)]);
                         }
                     }
+                    None => self.unknown_abis.extend(vec![abi_string]),
                 }
-                _ => {}
-            },
-            _ => {}
-        };    
+            }
+        }
     }
 
     fn check_item(&mut self, cx: &EarlyContext<'_>, it: &ast::Item) {
-        match &it.kind {
-            ast::ItemKind::ForeignMod(fm) => {
-                let fm: &ast::ForeignMod = fm;
-                let abi_string = match fm.abi {
-                    Some(abi) => abi.symbol_unescaped.as_str().to_string().replace("\"", ""),
-                    None => "C".to_string(),
-                };
-                if !is_internal_abi(lookup(&abi_string).unwrap()) {
-                    let items: &Vec<rustc_ast::ptr::P<rustc_ast::ast::ForeignItem>> = &fm.items;
-                    let session = &cx.sess();
-                    for item in items {
-                        match item.kind {
-                            rustc_ast::ast::ForeignItemKind::Fn(_) => {
-                                self.foreign_function_abis
-                                    .entry(abi_string.to_string())
-                                    .and_modify(|e| e.extend(vec![span_to_string(item.span, session)]))
-                                    .or_insert(vec![span_to_string(item.span, session)]);
-                            }
-                            rustc_ast::ast::ForeignItemKind::Static(_, _, _) => {
-                                self.static_item_abis
-                                    .entry(abi_string.to_string())
-                                    .and_modify(|e| e.extend(vec![span_to_string(item.span, session)]))
-                                    .or_insert(vec![span_to_string(item.span, session)]);
-                            }
-                            _ => {}
+        if let ast::ItemKind::ForeignMod(fm) = &it.kind {
+            let fm: &ast::ForeignMod = fm;
+            let abi_string = match fm.abi {
+                Some(abi) => abi.symbol_unescaped.as_str().to_string().replace('\"', ""),
+                None => "C".to_string(),
+            };
+            if !is_internal_abi(lookup(&abi_string).unwrap()) {
+                let items = &fm.items;
+                let session = &cx.sess();
+                for item in items {
+                    match item.kind {
+                        rustc_ast::ast::ForeignItemKind::Fn(_) => {
+                            self.foreign_function_abis
+                                .entry(abi_string.to_string())
+                                .and_modify(|e| e.extend(vec![span_to_string(item.span, session)]))
+                                .or_insert(vec![span_to_string(item.span, session)]);
                         }
+                        rustc_ast::ast::ForeignItemKind::Static(_, _, _) => {
+                            self.static_item_abis
+                                .entry(abi_string.to_string())
+                                .and_modify(|e| e.extend(vec![span_to_string(item.span, session)]))
+                                .or_insert(vec![span_to_string(item.span, session)]);
+                        }
+                        _ => {}
                     }
                 }
             }
-            _ => {}
         }
     }
 
