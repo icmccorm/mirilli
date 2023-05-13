38b699f1c084c22810b9851c179bb77858c6635e
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-05-13 13:35:56 -0400

    Merged external-ffi-binding crates into abi_subset.csv

--- scripts/extract_abi_subset.r
+++ scripts/extract_abi_subset.r
@@ -19,11 +19,29 @@ all <- read_csv(
     show_col_types = FALSE
 )
 
+external_ffi_bindings_path <- file.path("./data/external_ffi_bindings.csv")
+external_ffi_bindings <- read_csv(
+    external_ffi_bindings_path,
+    show_col_types = FALSE
+)
 
 late_names <- late_abis %>% select(crate_name)
 early_names <- early_abis %>% select(crate_name)
-bind_rows(late_names, early_names) %>%
-    unique %>%
+captured_abi_subset <- bind_rows(late_names, early_names) %>%
+    unique() %>%
     inner_join(all, by = ("crate_name")) %>%
     write_csv(file.path("./data/abi_subset.csv"))
+
+# merge all rows from captured abi subset with external ffi bindings and keep unique ones
+
+unmerged_output_path <- file.path("./data/captured_abi_subset.csv")
+
+captured_abi_subset %>% write_csv(unmerged_output_path)
+
+output_path <- file.path("./data/abi_subset.csv")
+
+combined <- bind_rows(captured_abi_subset, external_ffi_bindings) %>%
+    unique() %>%
+    write_csv(output_path)
+
 problems()
