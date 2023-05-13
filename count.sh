4ce52d766617219b0eb5c9dab4051d0a3b88f299
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-05-13 16:07:41 -0400

    Updated count.sh to ensure only *.rs files are included.

--- count.sh
+++ count.sh
@@ -7,7 +7,7 @@ rm -rf ./extracted
 rm -rf ./data/results/count/
 mkdir -p ./data/results
 mkdir -p ./data/results/count
-echo crate_name,version,ffi_c_count,ffi_count,test_count,bench_count >> ./data/results/testcount/count.csv
+echo crate_name,version,ffi_c_count,ffi_count,test_count,bench_count >> ./data/results/count/count.csv
 TRIES_REMAINING=3
 while IFS=, read -r name version; 
 do
@@ -15,22 +15,20 @@ do
     EXITCODE=$?
     if [ "$EXITCODE" -eq "0" ]; then 
         echo "SUCCESS"
-        cd extracted
-        NUM_TESTS_SRC=$(grep -r "#\[test\]" ./src | wc -l | xargs)
-        NUM_TESTS_TESTS=$(grep -r "#\[test\]" ./tests | wc -l | xargs)
-        NUM_TESTS_BENCHES=$(grep -r "#\[test\]" ./benches | wc -l | xargs)
+        NUM_TESTS_SRC=$(grep -r "#\[test\]" ./extracted/src | wc -l | xargs)
+        NUM_TESTS_TESTS=$(grep -r "#\[test\]" ./extracted/tests | wc -l | xargs)
+        NUM_TESTS_BENCHES=$(grep -r "#\[test\]" ./extracted/benches | wc -l | xargs)
         NUM_TESTS=$(($NUM_TESTS_SRC + $NUM_TESTS_TESTS + $NUM_TESTS_BENCHES))
 
-        NUM_BENCHES_SRC=$(grep -r "#\[bench\]" ./src | wc -l | xargs)
-        NUM_BENCHES_TESTS=$(grep -r "#\[bench\]" ./tests | wc -l | xargs)
-        NUM_BENCHES_BENCHES=$(grep -r "#\[bench\]" ./benches | wc -l | xargs)
+        NUM_BENCHES_SRC=$(grep -r "#\[bench\]" ./extracted/src | wc -l | xargs)
+        NUM_BENCHES_TESTS=$(grep -r "#\[bench\]" ./extracted/tests | wc -l | xargs)
+        NUM_BENCHES_BENCHES=$(grep -r "#\[bench\]" ./extracted/benches | wc -l | xargs)
         NUM_BENCHES=$(($NUM_BENCHES_SRC + $NUM_BENCHES_TESTS + $NUM_BENCHES_BENCHES))
-        NUM_FFI_C=$(grep -r 'extern\s*\(\"\(C\)\(-unwind\)\?\"\)\?\s*\(fn\|{\)' *.rs | wc -l | xargs)
-        NUM_FFI=$(grep -r 'extern\s*\(\"\(C\|cdecl\|stdcall\|fastcall\|vectorcall\|thiscall\|aapcs\|win64\|sysv64\|ptx-kernel\|msp430-interrupt\|x86-interrupt\|amdgpu-kernel\|efiapi\|avr-interrupt\|avr-non-blocking-interrupt\|C-cmse-nonsecure-call\|wasm\|system\|platform-intrinsic\|unadjusted\)\(-unwind\)\?\"\)\?\s*\(fn\|{\)' *.rs | wc -l | xargs)
-        cd ..
-        echo "$name,$version,$NUM_FFI_C,$NUM_FFI,$NUM_TESTS,$NUM_BENCHES" >> ./data/results/testcount/count.csv
+        NUM_FFI_C=$(grep -r -l 'extern\s*\(\"\(C\)\(-unwind\)\?\"\)\?\s*\(fn\|{\)' --include '*.rs' | wc -l | xargs)
+        NUM_FFI=$(grep -r -l 'extern\s*\(\"\(C\|cdecl\|stdcall\|fastcall\|vectorcall\|thiscall\|aapcs\|win64\|sysv64\|ptx-kernel\|msp430-interrupt\|x86-interrupt\|amdgpu-kernel\|efiapi\|avr-interrupt\|avr-non-blocking-interrupt\|C-cmse-nonsecure-call\|wasm\|system\|platform-intrinsic\|unadjusted\)\(-unwind\)\?\"\)\?\s*\(fn\|{\)' --include '*.rs' | wc -l | xargs)
+        echo "$name,$version,$NUM_FFI_C,$NUM_FFI,$NUM_TESTS,$NUM_BENCHES" >> ./data/results/count/count.csv
     else
-        echo "$name,$version,$EXITCODE" >> "./data/results/testcount/failed_download.csv"
+        echo "$name,$version,$EXITCODE" >> "./data/results/count/failed_download.csv"
     fi
     rm -rf ./extracted
 done <<< "$(tail -n +2 "$1")"
