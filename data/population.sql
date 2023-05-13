30eb2f6fe122891199e63755375fb314ad30c7fb
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-05-13 14:17:01 -0400

    Added crates_with_ffi view.

--- data/population.sql
+++ data/population.sql
@@ -1,17 +1,19 @@
-DROP VIEW IF EXISTS population;
 DROP VIEW IF EXISTS depends_on;
+DROP VIEW IF EXISTS population;
+DROP TABLE IF EXISTS sample;
 
 CREATE VIEW population AS (
     select t1.crate_id,
-        crates.name,
+        crates.name as crate_name,
         t2.version_id,
-        t2.num
+        t2.num as version
     from (
             select crate_id,
                 max(created_at) as created_at
             from versions
-            group by crate_id
             where not yanked
+            group by crate_id
+
         ) as t1
         INNER JOIN (
             select crate_id,
@@ -27,11 +29,22 @@ CREATE VIEW population AS (
 CREATE VIEW depends_on AS (
     SELECT parent.name as parent_name,
         parent.id as parent_id,
-        parent_v.num,
+        parent_v.version,
         child.name as child_name
     FROM dependencies
         INNER JOIN crates child on dependencies.crate_id = child.id
         INNER JOIN population parent_v on dependencies.version_id = parent_v.version_id
         INNER JOIN crates parent on parent_v.crate_id = parent.id
 );
+CREATE TABLE sample (
+    crate_name VARCHAR,
+    version VARCHAR
+);
+COPY sample(crate_name, version)
+FROM '/Users/icmccorm/git/ffickle/data/captured_abi_subset.csv'
+DELIMITER ','
+CSV HEADER;
 
+CREATE VIEW crates_with_ffi AS (
+select sample.crate_name, p.crate_id, sample.version, p.version_id from sample inner join population p on sample.crate_name = p.crate_name and sample.version = p.version
+                               );
