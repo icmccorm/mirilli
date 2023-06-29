1cbd55d7c839da1279f18a0a845a010befa4c02f
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-06-29 15:07:09 -0400

    Fixed a few variable names.

--- find_tests.sh
+++ find_tests.sh
@@ -8,11 +8,11 @@ touch ./data/results/tests/failed_rustc_compilation.csv
 echo "crate_name,version,exit_code" >> ./data/results/tests/failed_rustc_compilation.csv
 touch ./data/results/tests/failed_miri_compilation.csv
 echo "crate_name,version,exit_code" >> ./data/results/tests/failed_miri_compilation.csv
-while IFS=, read -r name version; 
+while IFS=, read -r crate_name version; 
 do
     TRIES_REMAINING=3
     while [ "$TRIES_REMAINING" -gt "0" ]; do
-        cargo-download -x "$name==$version" --output ./extracted
+        cargo-download -x "$crate_name==$version" --output ./extracted
         EXITCODE=$?
         TRIES_REMAINING=$(( TRIES_REMAINING - 1 ))
         if [ "$EXITCODE" -eq "0" ]; then 
@@ -24,33 +24,33 @@ do
             timeout 5m cargo test -q -- --list | sed 's/: test$//' | sed 's/^\(.*\) -.*(line \([0-9]*\))/\1 \2/' > rustc_list.txt
             RUSTC_FAILED=$PIPESTATUS
             if [ $RUSTC_FAILED -ne 0 ]; then
-                echo "$name,$version,$RUSTC_FAILED" >> "../data/results/tests/failed_rustc_compilation.csv"
+                echo "$crate_name,$version,$RUSTC_FAILED" >> "../data/results/tests/failed_rustc_compilation.csv"
             fi
             if [ $RUSTC_FAILED -eq 0 ]; then
                 timeout 5m cargo miri test -q -- --list | sed 's/: test$//' | sed 's/^\(.*\) -.*(line \([0-9]*\))/\1 \2/' > miri_list.txt
                 MIRI_FAILED=$PIPESTATUS
                 if [ $MIRI_FAILED -ne 0 ]; then
-                    echo "$name,$version,$MIRI_FAILED" >> "../data/results/tests/failed_miri_compilation.csv"
+                    echo "$crate_name,$version,$MIRI_FAILED" >> "../data/results/tests/failed_miri_compilation.csv"
                 fi            
             fi
             # if miri and rustc succeeded
             if [ "$MIRI_FAILED" -eq "0" ] && [ "$RUSTC_FAILED" -eq "0" ] && [ $(cat ./rustc_list.txt | wc -l) -ne 0 ]; then
-                OUTPUT_FILE="../data/results/tests/info/$name-$version.csv"
+                OUTPUT_FILE="../data/results/tests/info/$crate_name-$version.csv"
                 echo "exit_code,failed_from_ffi,test_name" > $OUTPUT_FILE
                 comm -13 ./miri_list.txt ./rustc_list.txt | sed 's/^/-1,-1,/' >> $OUTPUT_FILE
-                while IFS=" " read -r name line; 
+                while IFS=" " read -r test_name line; 
                 do
                     touch err
                     if [ -z "$line" ]; then  
-                        echo "Running $name..."
-                        MIRIFLAGS=-Zmiri-disable-isolation timeout 1m cargo miri test -q "$name" -- --exact 2> err
+                        echo "Running $test_name..."
+                        MIRIFLAGS=-Zmiri-disable-isolation timeout 1m cargo miri test -q "$test_name" -- --exact 2> err
                     else 
-                        echo "Running $name $line..."
-                        MIRIFLAGS=-Zmiri-disable-isolation timeout 1m cargo miri test -q -- --doc "$name $line" 2> err
+                        echo "Running $test_name $line..."
+                        MIRIFLAGS=-Zmiri-disable-isolation timeout 1m cargo miri test -q -- --doc "$test_name $line" 2> err
                     fi
                     EXITCODE=$?
                     grep -q "error: unsupported operation: can't call foreign function" ./err
-                    echo "$EXITCODE,$?,$name $line" >> $OUTPUT_FILE
+                    echo "$EXITCODE,$?,$test_name $line" >> $OUTPUT_FILE
                     rm -f err
                 done < ./miri_list.txt
             fi
@@ -58,7 +58,7 @@ do
         else
             echo "FAILED (exit $EXITCODE)"
             if [ "$TRIES_REMAINING" -eq "0" ]; then
-                echo "$name,$version,$EXITCODE" >> "./data/results/tests/failed_download.csv"
+                echo "$crate_name,$version,$EXITCODE" >> "./data/results/tests/failed_download.csv"
             else
                 echo "PAUSE..."
                 sleep 10
@@ -70,4 +70,4 @@ do
     rm -rf ./extracted
     TRIES_REMAINING=3
 done <<< "$(tail -n +2 "$1")"
-printf 'FINISHED! %s\n' "$name"
+printf 'FINISHED! %s\n' "$crate_name"
