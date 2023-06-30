50ca584cf0198b7951734cd6abb82ee07b09361f
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-06-29 20:27:53 -0400

    Updated script to remove duplicates.

--- find_tests.sh
+++ find_tests.sh
@@ -13,45 +13,53 @@ while IFS=, read -r crate_name version;
 do
     TRIES_REMAINING=3
     while [ "$TRIES_REMAINING" -gt "0" ]; do
+        echo "Downloading $crate_name@$version..."
         cargo-download -x "$crate_name==$version" --output ./extracted
         EXITCODE=$?
         TRIES_REMAINING=$(( TRIES_REMAINING - 1 ))
         if [ "$EXITCODE" -eq "0" ]; then 
-            echo "SUCCESS"
+            echo "Download successful"
             TRIES_REMAINING=0
             MIRI_FAILED=0
             RUSTC_FAILED=0
-            cd extracted
+            cd extracted || return
+            echo "Getting test list from rustc"
             timeout 5m cargo test -q -- --list | sed 's/: test$//' | sed 's/^\(.*\) -.*(line \([0-9]*\))/\1 \2/' > rustc_list.txt
-            RUSTC_FAILED=$PIPESTATUS
-            if [ $RUSTC_FAILED -ne 0 ]; then
+            RUSTC_FAILED=${PIPESTATUS[0]}
+            if [ "$RUSTC_FAILED" -ne 0 ]; then
+                echo "Failed to get test list from rustc"
                 echo "$crate_name,$version,$RUSTC_FAILED" >> "../data/results/tests/failed_rustc_compilation.csv"
             fi
-            if [ $RUSTC_FAILED -eq 0 ]; then
+            if [ "$RUSTC_FAILED" -eq 0 ]; then
+                echo "Getting test list from Miri"
                 timeout 5m cargo miri test -q -- --list | sed 's/: test$//' | sed 's/^\(.*\) -.*(line \([0-9]*\))/\1 \2/' > miri_list.txt
-                MIRI_FAILED=$PIPESTATUS
-                if [ $MIRI_FAILED -ne 0 ]; then
+                MIRI_FAILED=${PIPESTATUS[0]}
+                if [ "$MIRI_FAILED" -ne 0 ]; then
+                    echo "Failed to get test list from Miri"
                     echo "$crate_name,$version,$MIRI_FAILED" >> "../data/results/tests/failed_miri_compilation.csv"
                 fi            
             fi
             # if miri and rustc succeeded
-            if [ "$MIRI_FAILED" -eq "0" ] && [ "$RUSTC_FAILED" -eq "0" ] && [ $(cat ./rustc_list.txt | wc -l) -ne 0 ]; then
+            if [ "$MIRI_FAILED" -eq "0" ] && [ "$RUSTC_FAILED" -eq "0" ] && [ "$(wc -l < ./rustc_list.txt)" -ne 0 ]; then
+                
                 OUTPUT_FILE="../data/results/tests/info/$crate_name-$version.csv"
-                echo "exit_code,failed_from_ffi,test_name" > $OUTPUT_FILE
-                comm -13 ./miri_list.txt ./rustc_list.txt | sed 's/^/-1,-1,/' >> $OUTPUT_FILE
-                while IFS=" " read -r test_name line; 
+                echo "Logging tests to $OUTPUT_FILE"
+                echo "exit_code,failed_from_ffi,test_name" > "$OUTPUT_FILE"
+                comm -13 ./miri_list.txt ./rustc_list.txt | sed 's/^/-1,-1,/' >> "$OUTPUT_FILE"
+                while IFS=$'\n' read -r test_name; 
                 do
                     touch err
-                    if [ -z "$line" ]; then  
-                        echo "Running $test_name..."
+                    # if $test_name is of the form "filename line_number"
+                    if [[ $test_name =~ ^[a-zA-Z0-9_]*\ [0-9]*$ ]]; then
+                        echo "Running DOC test, $test_name..."
+                        MIRIFLAGS=-Zmiri-disable-isolation timeout 30s cargo miri test -q -- --doc "$test_name" 2> err
+                    else
+                        echo "Running NORMAL test, $test_name..."
                         MIRIFLAGS=-Zmiri-disable-isolation timeout 30s cargo miri test -q "$test_name" -- --exact 2> err
-                    else 
-                        echo "Running $test_name $line..."
-                        MIRIFLAGS=-Zmiri-disable-isolation timeout 30s cargo miri test -q -- --doc "$test_name $line" 2> err
                     fi
                     EXITCODE=$?
                     grep -q "error: unsupported operation: can't call foreign function" ./err
-                    echo "$EXITCODE,$?,$test_name $line" >> $OUTPUT_FILE
+                    echo "$EXITCODE,$?,$test_name" >> "$OUTPUT_FILE"
                     rm -f err
                 done < ./miri_list.txt
             fi
