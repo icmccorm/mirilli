fd66eeef859b07dc75c082b0e92cd3ee74b10e84
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-06-30 18:09:35 -0400

    Removed call to get test list from miri.

--- find_tests.sh
+++ find_tests.sh
@@ -21,7 +21,6 @@ do
         if [ "$EXITCODE" -eq "0" ]; then 
             echo "Download successful"
             TRIES_REMAINING=0
-            MIRI_FAILED=0
             RUSTC_FAILED=0
             cd extracted || return
             echo "Getting test list from rustc"
@@ -31,38 +30,43 @@ do
                 echo "Failed to get test list from rustc"
                 echo "$crate_name,$version,$RUSTC_FAILED" >> "../data/results/tests/failed_rustc_compilation.csv"
             fi
-            if [ "$RUSTC_FAILED" -eq 0 ]; then
-                echo "Getting test list from Miri"
-                timeout 5m cargo miri test -q -- --list | sed 's/: test$//' | sed 's/^\(.*\) -.*(line \([0-9]*\))/\1 \2/' > miri_list.txt
-                MIRI_FAILED=${PIPESTATUS[0]}
-                if [ "$MIRI_FAILED" -ne 0 ]; then
-                    echo "Failed to get test list from Miri"
-                    echo "$crate_name,$version,$MIRI_FAILED" >> "../data/results/tests/failed_miri_compilation.csv"
-                fi            
-            fi
             # if miri and rustc succeeded
-            if [ "$MIRI_FAILED" -eq "0" ] && [ "$RUSTC_FAILED" -eq "0" ] && [ "$(wc -l < ./rustc_list.txt)" -ne 0 ]; then
-                
+            if [ "$RUSTC_FAILED" -eq "0" ] && [ "$(wc -l < ./rustc_list.txt)" -ne 0 ]; then
                 OUTPUT_FILE="../data/results/tests/info/$crate_name-$version.csv"
                 echo "Logging tests to $OUTPUT_FILE"
-                echo "exit_code,failed_from_ffi,test_name" > "$OUTPUT_FILE"
-                comm -13 ./miri_list.txt ./rustc_list.txt | sed 's/^/-1,-1,/' >> "$OUTPUT_FILE"
+                echo "exit_code,had_ffi,test_name" > "$OUTPUT_FILE"
                 while read -r test_name; 
                 do
                     touch err
                     # if $test_name is of the form "filename line_number"
-                    if [[ $test_name =~ ^[a-zA-Z0-9_]*\ [0-9]*$ ]]; then
+                    if [[ $test_name =~ ^.*\ [0-9]*$ ]]; then
                         echo "Running DOC test, $test_name..."
-                        MIRIFLAGS=-Zmiri-disable-isolation timeout 30s cargo miri test -q -- --doc "$test_name" 2> err
+                        OUTPUT=$(MIRIFLAGS=-Zmiri-disable-isolation timeout 30s cargo miri test -q -- --doc "$test_name" 2> err)
                     else
                         echo "Running NORMAL test, $test_name..."
-                        MIRIFLAGS=-Zmiri-disable-isolation timeout 30s cargo miri test -q "$test_name" -- --exact 2> err
+                        OUTPUT=$(MIRIFLAGS=-Zmiri-disable-isolation timeout 30s cargo miri test -q "$test_name" -- --exact 2> err)
                     fi
                     EXITCODE=$?
-                    grep -q "error: unsupported operation: can't call foreign function" ./err
-                    echo "$EXITCODE,$?,$test_name" >> "$OUTPUT_FILE"
-                    rm -f err
-                done < ./miri_list.txt
+                    if [ "$EXITCODE" -ne 0 ]; then
+                        grep -q "error: unsupported operation: can't call foreign function" ./err
+                        HAD_FFI=$?
+                        if [ "$HAD_FFI" -eq "0" ]; then
+                            echo "Miri found FFI call for $test_name"
+                        else
+                            echo "Miri failed for $test_name."
+                        fi
+                    else
+                        HAD_FFI=0
+                        if echo $OUTPUT | grep -q "1 passed"; then
+                            echo "Miri passed for $test_name"
+                        else
+                            echo "Miri disabled for $test_name"
+                            EXITCODE="-1"
+                        fi
+                    fi
+                    echo "$EXITCODE,$HAD_FFI,$test_name" >> "$OUTPUT_FILE"
+                    rm -f err 
+                done < ./rustc_list.txt
             fi
             cd ..
         else
