b04885d3eca1bd3af14603c3bce276bf1c2be0b9
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-07-30 12:23:03 -0400

    Finished test list.

--- scripts/extract_miri_tests.r
+++ scripts/extract_miri_tests.r
@@ -6,7 +6,14 @@ if (file.exists("./data/results/tests/tests.csv")) {
         file.path("./data/results/tests/tests.csv"),
         show_col_types = FALSE
     )
-    tests %>% filter(had_ffi == 1, exit_code == 1) %>%
-        select(test_name, crate_name, version) %>%
+    tests %>% filter(had_ffi == 0, exit_code == 1) %>%
+        select(test_name, crate_name) %>%
         write_csv(file.path("./data/compiled/tests_failed_miri_ffi.csv"))
+    total_visited <- tests %>% nrow()
+    tests %>% filter(had_ffi==1, exit_code==124)
+    tests %>% group_by(had_ffi, exit_code) %>% summarize(n=round(n()/total_visited * 100, 1))
+
+    tests %>% filter(had_ffi == 0, exit_code == 1) %>% group_by(crate_name) %>% summarize(n=n()) %>%
+        arrange(desc(n)) %>% head(10)
+
 }
