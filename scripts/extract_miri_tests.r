58240c7bada048292263bad85c9e1f9b565ae51a
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-07-30 12:32:01 -0400

    Finalized sampling.

--- scripts/extract_miri_tests.r
+++ scripts/extract_miri_tests.r
@@ -6,14 +6,23 @@ if (file.exists("./data/results/tests/tests.csv")) {
         file.path("./data/results/tests/tests.csv"),
         show_col_types = FALSE
     )
-    tests %>% filter(had_ffi == 0, exit_code == 1) %>%
+    population <- read_csv(
+        file.path("./data/all.csv"),
+        show_col_types = FALSE
+    )
+    failed_miri_ffi <- tests %>%
+        filter(had_ffi == 0, exit_code == 1) %>%
         select(test_name, crate_name) %>%
-        write_csv(file.path("./data/compiled/tests_failed_miri_ffi.csv"))
-    total_visited <- tests %>% nrow()
-    tests %>% filter(had_ffi==1, exit_code==124)
-    tests %>% group_by(had_ffi, exit_code) %>% summarize(n=round(n()/total_visited * 100, 1))
+        inner_join(population, by = c("crate_name"))
 
-    tests %>% filter(had_ffi == 0, exit_code == 1) %>% group_by(crate_name) %>% summarize(n=n()) %>%
-        arrange(desc(n)) %>% head(10)
-
+    # find the sample size needed for a 95% confidence interval
+    # with a population size equal to the number of rows in failed_miri_ffi
+    # and a margin of error of 5%
+    sample_size <- ceiling(
+        (1.96^2 * nrow(failed_miri_ffi) * 0.5 * 0.5) / (0.05^2 * (nrow(failed_miri_ffi) - 1) + 1.96^2 * 0.5 * 0.5)
+    )
+    failed_miri_ffi %>%
+        sample_n(sample_size) %>%
+        write_csv(file.path("./data/compiled/tests_failed_miri_sample.csv"))
+    failed_miri_ffi %>% write_csv(file.path("./data/compiled/tests_failed_miri_ffi.csv"))
 }
