40af93079b5b1f61b4487fe90d7c1b021e8eadce
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-08-12 13:39:11 -0400

    Added initial failed test results.

--- scripts/summarize_exec.r
+++ scripts/summarize_exec.r
@@ -11,6 +11,20 @@ if (file.exists("./data/results/execution/errors.csv")) {
         summarise(count = n()) %>%
         arrange(desc(count)) %>%
         write_csv(file.path("./data/compiled/error_summary.csv"))
+    ub <- errors %>% 
+        filter(error_type == "Undefined Behavior") %>%
+        unique()
+    ub$ub_type <- NA
+    ub$ub_type[grepl("trying to retag", ub$error_text)] <- "Stacked Borrows - Failed Retag"
+    ub$ub_type[grepl("was dereferenced after this allocation got freed", ub$error_text)] <- "Use-After-Free"
+    ub$ub_type[grepl("attempting a write access", ub$error_text)] <- "Stacked Borrows - Failed Write"
+    ub$ub_type[grepl("attempting a read access", ub$error_text)] <- "Stacked Borrows - Failed Read"
+    ub$ub_type[grepl("constructing invalid value", ub$error_text)] <- "Constructing Invalid Value"
+    ub$ub_type[grepl("unwinding past the topmost frame of the stack ", ub$error_text)] <- "Invalid Unwinding"
+    ub$ub_type[is.na(ub$ub_type)] <- "Internal Error"
+    ub_crates_to_investigate <- ub %>% 
+        select(crate, ub_type, test) %>%
+        write_csv(file.path("./data/compiled/tests_with_ub.csv"))
     errors %>% 
         group_by(error_text, error_type, error_subtext) %>%
         slice(1) %>%
