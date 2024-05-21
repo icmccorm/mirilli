d7e400806c6723ac38e4c5195aae07ac0518ebc6
Ian McCormack <icmccorm@andrew.cmu.edu>
2024-05-21 11:46:51 -0400

    Added .Rproj and removed additional TQDM dependency.

--- scripts/stage3/parse.py
+++ scripts/stage3/parse.py
@@ -1,11 +1,9 @@
 import os
 import sys
-import json
 import re
 import parse_tb
 import parse_shared
 import parse_sb
-from tqdm import tqdm
 
 def read_flags(FLAGS_CSV_PATH):
     with open(FLAGS_CSV_PATH, "r") as f:
@@ -202,7 +200,7 @@ tree_summary = open_csv(root_dir, "tree_summary.csv", ["crate_name", "test_name"
 stack_summary = open_csv(root_dir, "stack_summary.csv", ["crate_name", "test_name"] + parse_shared.COLUMNS)
 
 print(f"Processing errors from '{os.path.basename(base)}' mode...")
-for crate in tqdm(os.listdir(data_dir)):
+for crate in os.listdir(data_dir):
     stack_dir = os.path.join(data_dir, crate, "stack")
     tree_dir = os.path.join(data_dir, crate, "tree")
     crate_name = os.path.basename(crate)
