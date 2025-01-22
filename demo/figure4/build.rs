0cb25588bdca01bf328acf4bda180b5c4101d499
Ian McCormack <icmccorm@andrew.cmu.edu>
2025-01-22 14:08:20 -0500

    Removed unnecessary dependencies.

--- demo/figure4/build.rs
+++ demo/figure4/build.rs
@@ -1,7 +1,4 @@
 extern crate cc;
-use std::path::PathBuf;
-use std::env;
 fn main() {
-    println!("cargo:rustc-link-search=./src/");
     cc::Build::new().flag("-O0").file("src/main.c").compile("libfigure4.a");
 }
